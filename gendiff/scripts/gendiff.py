e5329905966a53a36495d81255e20d6203b530a6
ivan <ivan@1s-MacBook-Pro.local>
2020-10-03 16:29:37 +0300

    Changed minor typo.

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,10 +7,8 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    # parser.add_argument('-f')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
 
 
 if __name__ == '__main__':
