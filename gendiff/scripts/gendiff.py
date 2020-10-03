fc9c19b7c5e0d18fe43c98e66a81886a0678dd6a
ivan <ivan@1s-MacBook-Pro.local>
2020-10-03 15:46:00 +0300

    Added solution on 2 step.

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,8 +8,9 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     # parser.add_argument('-f')
-    parser.add_argument('-F --format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
+    print(args.accumulate(args.integers))
 
 
 if __name__ == '__main__':
