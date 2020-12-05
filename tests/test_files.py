dd87a58ec7dd072bdc494b9a4052d004ae4a368c
ivekhov <ivekhov@gmail.com>
2020-12-05 14:12:33 +0300

    Change test function: put correct answer in separate file

--- tests/test_files.py
+++ tests/test_files.py
@@ -5,15 +5,14 @@
 from gendiff.scripts.gendiff import generate_diff
 
 
+def read_correct(filename):
+	with open(filename, 'r') as f:
+		answer = f.readlines()
+	return ''.join(answer)
+
+
 def test_output():
 	"""Check output."""
 	output = generate_diff('./tests/fixtures/file01.json', './tests/fixtures/file02.json')
-	expected = """{
-  - follow: False
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: True
-}"""
+	expected = read_correct('./tests/fixtures/test_01_correct.txt')
 	assert output == expected
