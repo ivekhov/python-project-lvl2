5827df17936fd75f5f85f99c75adf0c7f1489eaf
ivekhov <ivekhov@gmail.com>
2022-05-18 22:33:21 +0300

    Add initial commit.

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Script for gendiff start."""
+
+
+from gendiff.tools.gendiff_help import gendiff_help
+
+
+def main():
+    """."""
+    gendiff_help()
+
+
+if __name__ == '__main__':
+    main()
---
+++ gendiff/tools/gendiff_help.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Script for gendiff help."""
+
+import argparse
+
+
+def gendiff_help():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
