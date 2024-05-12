04bfce8058b3ae51c48ce8df4bba773cd67a33a4
Ivan Vekhov <ivekhov@gmail.com>
2024-05-13 02:20:15 +0300

    update json formatter

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,2 +1,5 @@
+import json
+
 def json(tree: list) -> list:
-    return f"{sorted(tree, key=lambda item: item.get('node'))}"
+    # return f"{sorted(tree, key=lambda item: item.get('node'))}"
+    return json.dumps(tree, indent=4)
