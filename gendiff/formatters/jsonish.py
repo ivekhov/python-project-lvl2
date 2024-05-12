3713fd70341aefc697f4373ee8f7008e3ed836cc
Ivan Vekhov <ivekhov@gmail.com>
2024-05-13 02:29:42 +0300

    fix code style in test

--- gendiff/formatters/jsonish.py
+++ gendiff/formatters/jsonish.py
@@ -2,5 +2,4 @@ import json
 
 
 def jsonish(tree: list) -> list:
-    # return f"{sorted(tree, key=lambda item: item.get('node'))}"
     return json.dumps(tree, indent=4)
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,7 +4,7 @@ from gendiff import generate_diff
 
 
 EXTENSIONS = [
-    'json', 
+    'json',
     'yml',
 ]
 
@@ -15,7 +15,7 @@ def get_fixture_path(file_name):
 
 def read_file_content(file_path):
     with open(file_path) as file:
-        return file.read() 
+        return file.read()
 
 
 @pytest.fixture
@@ -32,14 +32,21 @@ def test_stylish(correct_stylish):
     for ext in EXTENSIONS:
         file_before = get_fixture_path(f'file_before.{ext}')
         file_after = get_fixture_path(f'file_after.{ext}')
-        assert generate_diff(file_before, file_after, 'stylish') == correct_stylish
+        assert generate_diff(
+            file_before,
+            file_after, 'stylish'
+        ) == correct_stylish
 
 
 def test_plain(correct_plain):
     for ext in EXTENSIONS:
         file_before = get_fixture_path(f'file_before.{ext}')
         file_after = get_fixture_path(f'file_after.{ext}')
-        assert generate_diff(file_before, file_after, 'plain') == correct_plain
+        assert generate_diff(
+            file_before,
+            file_after,
+            'plain'
+        ) == correct_plain
 
 
 @pytest.fixture
@@ -56,11 +63,19 @@ def test_extended_stylish(correct_extended_stylish):
     for ext in EXTENSIONS:
         file_before = get_fixture_path(f'file_extended_before.{ext}')
         file_after = get_fixture_path(f'file_extended_after.{ext}')
-        assert generate_diff(file_before, file_after, 'stylish') == correct_extended_stylish
+        assert generate_diff(
+            file_before,
+            file_after,
+            'stylish'
+        ) == correct_extended_stylish
 
 
 def test_extended_plain(correct_extended_plain):
     for ext in EXTENSIONS:
         file_before = get_fixture_path(f'file_extended_before.{ext}')
         file_after = get_fixture_path(f'file_extended_after.{ext}')
-        assert generate_diff(file_before, file_after, 'plain') == correct_extended_plain
+        assert generate_diff(
+            file_before,
+            file_after,
+            'plain'
+        ) == correct_extended_plain
